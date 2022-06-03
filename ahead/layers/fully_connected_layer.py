Further attempts at refactoring. Exploding gradients when unusual architecture -- framework works but sucks to use. Their softmax backprop seems correct though.

ahead/layers/fully_connected_layer.py
@@ -35,7 +35,8 @@ class fullyConnectedLayer:
         '''
         self.input_size = input.shape
         self.flat = input.flatten()
-        
+                
+        self.Z = np.dot(self.flat, self.weights) + self.bias # Z = W*X+B
         # if self.operation == 'Softmax':
         #     (self.prediction, self.expZ) = self.soft(self.Z)
         # elif self.operation == 'ReLu':
@@ -45,44 +46,38 @@ class fullyConnectedLayer:
         # else:
         #     raise('Define a valid non-linear operation.')
         # return self.prediction
-        return np.dot(self.flat, self.weights) + self.bias
+        return self.Z
 
     def backward(self, grad0, learning_rate):
         '''
         :grad_out: previous layer gradient (numpy ndarray)
         :learning_rate: learning rate (float)
         '''
-
-        weights_gradient = self.flat[np.newaxis].T @ grad0[np.newaxis]
-        self.weights -= learning_rate * weights_gradient
-        self.bias -= learning_rate * grad0
-
-        input_gradient = np.dot(self.weights, grad0[np.newaxis].T)
-        return input_gradient.reshape(self.input_size)
-
         # for i, grad in enumerate(grad0): # On veut grad_out[i] et i.
         #     if grad == 0:
         #         continue
-        #     # grad = dL/dY, let's backpropagate.
-        #     expZ = np.exp(self.Z)
-        #     S = np.sum(expZ)
-        #     # print(self.Z)
-        #     dY_dZ = - expZ[i]*expZ/(S**2) # cas général
-        #     dY_dZ[i] = expZ[i]*(S - expZ[i]) / (S**2) # cas où on cherche le gradient de Yi par rapport à Zi
+        #     # # grad = dL/dY, let's backpropagate.
+        #     # expZ = np.exp(self.Z)
+        #     # S = np.sum(expZ)
+        #     # # print(self.Z)
+        #     # dY_dZ = - expZ[i]*expZ/(S**2) # cas général
+        #     # dY_dZ[i] = expZ[i]*(S - expZ[i]) / (S**2) # cas où on cherche le gradient de Yi par rapport à Zi
 
-        #     dZ_dW = self.flat
-        #     dZ_dX = self.weights # Z = W*X + B, X = input image en vecteur colonne.
-        #     dZ_dB = 1
+        dZ_dW = self.flat
+        dZ_dX = self.weights # Z = W*X + B, X = input image en vecteur colonne.
+        dZ_dB = 1
 
-        #     dL_dZ = grad*dY_dZ #dL_dZ = dL_dY * dY_dZ
+        dL_dZ = grad0 #dL_dZ = dL_dY * dY_dZ
 
-        #     dL_dW = dZ_dW[np.newaxis].T @ dL_dZ[np.newaxis] # newaxis nécessaire pour convertir des 
-        #     # np.array 1D en vecteur ligne et vecteur colonne.
-        #     dL_dX = dZ_dX @ dL_dZ 
-        #     dL_dB = dL_dZ * dZ_dB
+        dL_dW = dZ_dW[np.newaxis].T @ dL_dZ[np.newaxis] # newaxis nécessaire pour convertir des 
+        # np.array 1D en vecteur ligne et vecteur colonne.
+        dL_dX = dZ_dX @ dL_dZ 
+        dL_dB = dL_dZ * dZ_dB
 
-        #     # Update parameters
-        #     self.weights -= learning_rate*dL_dW
-        #     self.bias -= learning_rate*dL_dB
+            # Update parameters
+        self.weights -= learning_rate*dL_dW
+        self.bias -= learning_rate*dL_dB
+
+        # print(np.max(self.flat))
             
-        # return dL_dX.reshape(self.input_size)
+        return dL_dX.reshape(self.input_size)

ahead/layers/model.py
@@ -1,4 +1,3 @@
-from unicodedata import category
 import numpy as np
 
 from layers.convolutional_layer import convolutionalLayer
@@ -52,17 +51,6 @@ class model:
 
         return predictions
     
-    # def initialize_weights(self, input, label):
-    #     predictions = []
-    #     for n in range(len(self.layers)):
-    #         self.layers[n].initialize_weights()
-    #         output = self.layers[n].forward(input)
-    #         predictions.append(output)
-    #         if n != len(self.layers)-1:
-    #             flat = output.flatten()
-    #             self.layers[n+1].numInputs = flat.shape[0]
-    #         input = output
-
     def backward(self, input, lr):
         gradients = []
         for n in range(len(self.layers)):
@@ -72,24 +60,20 @@ class model:
 
         return gradients
 
-    def categorical_cross_entropy(self, y_true, y_pred):
-        return - np.log(y_pred[int(y_true)] + 1e-15)
+    def cross_entropy(self, y_pred, y_true):
+        loss = -np.sum(y_true * np.log(y_pred + 10**-100))
 
-    def train(self, input, label, lr): # 1 image by 1 image
-        # Full forward propagation
+        return loss
+
+    def train(self, input, label, lr):
         predictions = self.forward(input, label)
         results = predictions[-1]
 
-        # Categorical cross entropy
-        loss = self.categorical_cross_entropy(label, results)
-        # print(loss)
+        loss = self.cross_entropy(results, label)
         accuracy = 1 if np.argmax(results) == label else 0 # If the highest probability is for the correct label, accuracy = 1, else 0.
-        # Compute initial gradient
-        grad0 = np.zeros(self.num_classes) ## Number of classification categories
-        grad0[int(label)] = -1 / results[int(label)]
-        # print('Gradient : ', np.min(grad0))
-
-        # Full back propagation
+        
+        grad0 = np.zeros(self.num_classes)
+        grad0[int(label)] = - 1 / results[int(label)] if np.abs(-1/results[int(label)]) < 20 else -20 
         gradients = self.backward(grad0, lr)
 
         return predictions, gradients, loss, accuracy
@@ -98,10 +82,9 @@ class model:
         for epoch in range(num_epochs):
             print('Running Epoch : %d' % (epoch+1))
 
-            # Shuffle the training data
-            # shuffle_order = np.random.permutation(len(inputs))
-            # inputs = inputs[shuffle_order]
-            # labels = labels[shuffle_order]
+            shuffle_order = np.random.permutation(len(inputs))
+            inputs = inputs[shuffle_order]
+            labels = labels[shuffle_order]
 
             # Training
             x = []
@@ -117,12 +100,12 @@ class model:
                 # Update loss and accuracy
                 predictions, gradients, dL, dA = self.train(im, label, lr)
                 
-                bool = gradients[-1].any()
-                if i == 0:
-                    print(bool)
-                else:
-                    if not bool:
-                        print(bool)
+                # bool = gradients[-1].any()
+                # if i == 0:
+                #     print(bool)
+                # else:
+                #     if not bool:
+                #         print(bool)
                 
                 loss += dL
                 num_correct += dA

ahead/layers/softmaxLayer.py
@@ -6,7 +6,7 @@ class softmaxLayer:
         self.num_outputs = n_in
     
     def softmax(self, x):
-        y = np.exp(x)
+        y = np.exp(x - np.max(x))
         z = y/np.sum(y, axis = 0)
         return x, y, z
     
@@ -19,5 +19,5 @@ class softmaxLayer:
         z = self.output
         jacobian = z * np.identity(z.size) - z.transpose() @ z
         dL_dX = grad0 @ jacobian
-        print(grad0, dL_dX)
+        # print(grad0, dL_dX)
         return dL_dX

ahead/utils/dataReader.py
@@ -1,17 +1,17 @@
 ## DATASET
-import numpy as np
 
 def get_data(str, num, dims):
     if str == 'cifar10':
         from keras.datasets import cifar10
         (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-        x_train = np.squeeze(x_train[:num])
+        x_train = x_train[:num]
         y_train = y_train[:num]
-        x_test = np.squeeze(x_test[:num])
+        x_test = x_test[:num]
         y_test = y_test[:num]
 
         if dims != 0:
+            import numpy as np
             x_train = np.repeat(x_train[..., np.newaxis], dims, axis = -1)
             x_test = np.repeat(x_test[..., np.newaxis], dims, axis = -1)  
 
@@ -27,6 +27,7 @@ def get_data(str, num, dims):
         
         # put in kwargs
         if dims != 0:
+            import numpy as np
             x_train = np.repeat(x_train[..., np.newaxis], dims, axis = -1)
             x_test = np.repeat(x_test[..., np.newaxis], dims, axis = -1)
 